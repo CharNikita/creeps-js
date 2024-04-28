06015047ae84a775915f04f3ac560f314bb47cb6
goncharenko <goncharenkonikitae@gmail.com>
2024-04-28 23:54:32 +0300

    init

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -14,16 +14,26 @@ const roleUpgrader = {
         if (creep.memory.state === 'upgrading') {
             const upgradeResult = creep.upgradeController(creep.room.controller);
             if (upgradeResult === ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+                creep.moveTo(creep.room.controller, {
+                        visualizePathStyle: {
+                            stroke: '#ffffff'
+                        }
+                    }
+                );
             }
             return;
         }
         if (creep.memory.state === 'withdrawing') {
             const sources = creep.room.find(FIND_MY_SPAWNS);
             const withdrawResult = creep.withdraw(sources[0]);
-            console.log()
+            console.log(withdrawResult)
             if (withdrawResult === ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                creep.moveTo(sources[0], {
+                        visualizePathStyle: {
+                            stroke: '#ffaa00'
+                        }
+                    }
+                );
             }
             return;
         }
