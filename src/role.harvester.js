44cd6190b6862b66c7039d77bce670863e36c0e9
goncharenko <goncharenkonikitae@gmail.com>
2024-04-28 21:49:27 +0300

    init

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -1,26 +1,47 @@
 const roleHarvester = {
-
     /** @param {Creep} creep **/
     run: function (creep) {
-        if (creep.store.getFreeCapacity() > 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+        creep.memory.state = 'idle'
+        if (creep.store[RESOURCE_ENERGY] === 0) {
+            creep.memory.state = 'harvesting';
+            creep.say('harvesting');
+        }
+        if (creep.store.getFreeCapacity() === 0) {
+            creep.memory.state = 'transferring';
+            creep.say('transferring');
+        }
+
+        if (creep.memory.state === 'harvesting') {
+            const sources = creep.room.find(FIND_SOURCES);
+            const harvestResult = creep.harvest(sources[0]);
+            if (harvestResult === ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {
+                        visualizePathStyle: {
+                            stroke: '#ffffff'
+                        }
+                    }
+                );
             }
-        } else {
+            return;
+        }
+        if (creep.memory.state === 'transferring') {
             const targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_TOWER) &&
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    return (structure.structureType === STRUCTURE_EXTENSION
+                            || structure.structureType === STRUCTURE_SPAWN)
+                        && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
-            if (targets.length > 0) {
-                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+            const transferResult = creep.transfer(targets[0], RESOURCE_ENERGY);
+            if (transferResult === ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0], {
+                        visualizePathStyle: {
+                            stroke: '#ffaa00'
+                        }
+                    }
+                );
             }
+            return;
         }
     }
 };
