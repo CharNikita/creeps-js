384bf9c4d5ae922ea2222c06fe890e343f58103e
goncharenko <goncharenkonikitae@gmail.com>
2024-04-29 22:28:19 +0300

    init

--- src/spawner.js
+++ src/spawner.js
@@ -61,11 +61,11 @@ const spawner = {
             spawnCargo();
             return;
         }
-        if (_.filter(Game.creeps, (creep) => creep.memory.role === 'upgrader').length < 2) {
+        if (_.filter(Game.creeps, (creep) => creep.memory.role === 'upgrader').length < 3) {
             spawnUpgrader();
             return;
         }
-        if (_.filter(Game.creeps, (creep) => creep.memory.role === 'builder').length < 2) {
+        if (_.filter(Game.creeps, (creep) => creep.memory.role === 'builder').length < 1) {
             spawnBuilder();
             return;
         }
