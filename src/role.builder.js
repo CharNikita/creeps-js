1f8d7d3fa666df917736647e2132ab4c7857b8c6
goncharenko <goncharenkonikitae@gmail.com>
2024-04-29 22:26:32 +0300

    init

--- src/role.builder.js
+++ src/role.builder.js
@@ -22,8 +22,6 @@ const roleBuilder = {
                 filter: object => (object.structureType !== STRUCTURE_WALL) && (object.hits < object.hitsMax)
             });
 
-            targets.sort((a,b) => a.hits - b.hits);
-
             if(targets.length > 0) {
                 if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
