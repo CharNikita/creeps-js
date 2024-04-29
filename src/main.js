f80c06c663a4be27fa73e71f957f2f11330bfc52
goncharenko <goncharenkonikitae@gmail.com>
2024-04-29 22:05:22 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var roleCargo = require('role.cargo');
 var spawner = require('spawner');
 
 module.exports.loop = function () {
@@ -23,6 +24,9 @@ module.exports.loop = function () {
         }
         if (creep.memory.role === 'builder') {
             roleBuilder.run(creep);
+        }
+        if (creep.memory.role === 'cargo') {
+            roleCargo.run(creep);
         }
     }
 }
