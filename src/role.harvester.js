3f420dd5d00d299892a1041150c835a88d007602
goncharenko <goncharenkonikitae@gmail.com>
2024-04-30 20:05:43 +0300

    init

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -1,75 +1,81 @@
-const cleanUp = (creep) => {
-    const sourceId = creep.memory.sourceId;
-    if (sourceId) {
-        Memory.sources[sourceId].assignedCreepId = null;
-        creep.memory.sourceId = null;
-    }
-}
+const DIE = 'die';
+const HARVEST = 'harvest';
+const TRANSFER = 'transfer';
 
 const roleHarvester = {
     /** @param {Creep} creep **/
     run: function (creep) {
         if (creep.ticksToLive <= 3) {
-            creep.memory.state = 'die';
-            creep.say('die');
+            creep.memory.state = DIE;
         } else if (creep.store.getFreeCapacity() > 0) {
-            creep.memory.state = 'harvesting';
-            creep.say('harvesting');
+            creep.memory.state = HARVEST;
         } else if (creep.store.getFreeCapacity() === 0) {
-            creep.memory.state = 'transferring';
-            creep.say('transferring');
+            creep.memory.state = TRANSFER;
         }
 
-        if (creep.memory.state === 'harvesting') {
-            let target = Game.getObjectById(creep.memory.sourceId);
-            if (!target) {
-                target = creep.pos.findClosestByPath(FIND_SOURCES, {
-                    filter: function (source) {
-                        return Memory.sources[source.id].assignedCreepId == null;
+        if (creep.memory.state === HARVEST) {
+            this._withdraw(creep);
+        } else if (creep.memory.state === TRANSFER) {
+            this._transfer(creep);
+        } else if (creep.memory.state === DIE) {
+            this._cleanUp(creep);
+        }
+    },
+    /** @param {Creep} creep **/
+    _transfer: function (creep) {
+        this._cleanUp(creep);
+
+        const targets = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType === STRUCTURE_CONTAINER);
+            }
+        });
+
+        const transferResult = creep.transfer(targets, RESOURCE_ENERGY);
+        if (transferResult === ERR_NOT_IN_RANGE) {
+            creep.moveTo(targets, {
+                    visualizePathStyle: {
+                        stroke: '#ffaa00'
                     }
-                });
-                if (target === null) {
-                    return;
                 }
-                Memory.sources[target.id].assignedCreepId = creep.id;
-                creep.memory.sourceId = target.id;
-            }
-
-            const harvestResult = creep.harvest(target);
-            if (harvestResult === ERR_NOT_IN_RANGE) {
-                creep.moveTo(target, {
-                        visualizePathStyle: {
-                            stroke: '#ffffff'
-                        }
-                    }
-                );
-            }
+            );
+        }
+    },
+    /** @param {Creep} creep **/
+    _withdraw: (creep) => {
+        let target = this._getTargetToWithdraw(creep);
+        if (!target) {
             return;
         }
-        if (creep.memory.state === 'transferring') {
-            cleanUp(creep);
 
-            const targets = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType === STRUCTURE_CONTAINER);
+        const harvestResult = creep.harvest(target);
+        if (harvestResult === ERR_NOT_IN_RANGE) {
+            creep.moveTo(target, {
+                    visualizePathStyle: {
+                        stroke: '#ffffff'
+                    }
                 }
-            });
-            const transferResult = creep.transfer(targets, RESOURCE_ENERGY);
-            if (transferResult === ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets, {
-                        visualizePathStyle: {
-                            stroke: '#ffaa00'
-                        }
-                    }
-                );
+            );
+        }
+    },
+    /** @param {Creep} creep **/
+    _getTargetToWithdraw: (creep) => {
+        return creep.pos.findClosestByPath(FIND_SOURCES, {
+            filter: function (source) {
+                return Memory.sources[source.id].assignedCreepId == null;
             }
+        });
+    },
+    /** @param {Creep} creep **/
+    _cleanUp: (creep) => {
+        const sourceId = creep.memory.sourceId;
+        if (!sourceId) {
             return;
         }
-        if (creep.memory.state === 'die') {
-            cleanUp(creep);
-            return;
-        }
+        Memory.sources[sourceId].assignedCreepId = null;
+        creep.memory.sourceId = null;
     }
+
 };
 
 module.exports = roleHarvester;
