e9bcca0e45ef6616ba82b7b96846d72bb356b75d
goncharenko <goncharenkonikitae@gmail.com>
2024-04-30 19:32:35 +0300

    init

--- src/role.cargo.js
+++ src/role.cargo.js
@@ -13,7 +13,9 @@ const roleCargo = {
         if (creep.memory.state === 'transfer') {
             const transferTargets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType === STRUCTURE_SPAWN || structure.structureType === STRUCTURE_EXTENSION)
+                    return (structure.structureType === STRUCTURE_SPAWN
+                            || structure.structureType === STRUCTURE_EXTENSION
+                            || structure.structureType === STRUCTURE_TOWER)
                         && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
